be2e66f7ea1076b87cbf26171217fdb5d59ed420
Daniel Haag <danni.haag91@gmail.com>
2021-12-09 18:24:43 -0300

    primeiro commit

---
+++ MembrosEstaticosExe/ConversorDeMoeda.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MembrosEstaticosExe
+{
+    class ConversorDeMoeda
+    {
+        public static double IOF = 0.06;
+
+        public static double Converter(double cotacao, double dolares)
+        {
+            double reais = (dolares * cotacao);
+            reais = reais + (IOF * reais);
+            return reais;
+        }
+
+    }
+}
---
+++ MembrosEstaticosExe/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace MembrosEstaticosExe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cotacaoDolar = 0;
+            double dolares = 0;
+            double reais = 0;
+
+            Console.WriteLine("Qual é a cotação do dólar?");
+            cotacaoDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Quantos dólares você vai comprar?");
+            dolares = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            reais = ConversorDeMoeda.Converter(cotacaoDolar, dolares);
+
+            Console.WriteLine("Valor a ser pago em reais: " + reais);
+
+            Console.Read();
+        }
+    }
+}
